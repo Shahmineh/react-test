f7b18c989248876261f6c2ab5a502d2a0cfa1486
Shamin Krook <shahmineh.gh@hotmail.com>
2018-10-03 15:46:43 +0200

    Removed all info in components

--- src/components/Conditionals/Conditionals.js
+++
@@ -1,1 +0,0 @@
-export default class Conditionals extends Component {}
--- src/components/Loops/Loops.js
+++
@@ -1,7 +0,0 @@
-export default class Loops extends Component {
-  arr = [
-    {name: 'Cecilia', id: 1},
-    {name: 'Dan', id: 2},
-    {name: 'Eve', id: 3}
-  ];
-}
